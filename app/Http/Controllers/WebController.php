216f3f2fa0db2ec4409f982663e2dc75274cd8d8
dale <dale.hsu@dunqian.tw>
2022-06-04 00:19:32 +0800

    新增商品列表

---
+++ app/Http/Controllers/WebController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class WebController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('web.index', ['products' => $products]);
+    }
+}
---
+++ database/migrations/2022_06_03_153644_create_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->comment('商品名稱');
+            $table->string('content')->comment('商品說明');
+            $table->integer('price')->comment('價格');
+            $table->integer('quantity')->comment('數量');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
